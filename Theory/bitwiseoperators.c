52f73fb56d27947111c91dbbea84f2fc61a10cc2
Rodrigo - UCSP <Draouch@users.noreply.github.com>
2016-09-02 01:58:31 -0500

    Update bitwiseoperators.c

--- Theory/bitwiseoperators.c
+++ Theory/bitwiseoperators.c
@@ -3,31 +3,34 @@
 int main(){
 	
 	int a = 12, b = 14, c = -7;
+	/*a = 1100, b = 1110, c = -111*/
+	
+	----------------------------------------
 	
 	/*AND*/
 	
-	printf("AND = %d\n", a&b);
+	printf("AND = %d\n", a&b); /*12*/
 	
 	/*OR*/
 	
-	printf("OR = %d\n", a|b);
+	printf("OR = %d\n", a|b); /*14*/
 	
 	/*XOR*/
 	
-	printf("XOR = %d\n", a^b);
+	printf("XOR = %d\n", a^b); /*2*/
 	
 	/*Complement*/
 	
-	printf("Complement = %d\n",~a);
-	printf("Complement = %d\n",~c);
+	printf("Complement = %d\n",~a); /*-13*/
+	printf("Complement = %d\n",~c); /*6*/
 	
 	/*Left Shift*/
 	
-	printf("Left Shift = %d\n", a<<1);
+	printf("Left Shift = %d\n", a<<1); /*24*/
 	
 	/*Right Shift*/
 	
-	printf("Right Shift = %d\n", a>>1);
+	printf("Right Shift = %d\n", a>>1); /*6*/
 	
 	return 0;
 	
