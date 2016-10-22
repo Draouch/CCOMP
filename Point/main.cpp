9a478a6de68e022d9619ce0038ce8445b6549a35
Rodrigo - UCSP <Draouch@users.noreply.github.com>
2016-10-21 20:23:07 -0500

    Update main.cpp

--- Point/main.cpp
+++ Point/main.cpp
@@ -1,10 +1,67 @@
 #include "point.cpp"
-	
+
+using namespace std;
+
 int main(){
 	
-	PointArray n;
-	n.printArr();
+	Point x;
+	Point y;
 	
+	PointArray a;
+	
+	x.setX(5);
+	x.setY(8);
+	x.printPoint();
+	
+	cout<<"\n"<<endl;
+	
+	x.modifyPoint(4,7);
+	x.printPoint();
+	
+	cout<<"\n"<<endl;
+	
+	y.setX(4);
+	y.setY(9);
+	y.printPoint();
+	
+	cout<<"\n"<<endl;
+	
+	a.getSize();
+	
+	cout<<"\n"<<endl;
+	
+	
+	a.resize(5);
+	a.insert(0,x);
+	a.printArr();
+	
+	cout<<"\n"<<endl;
+	
+	a.getSize();
+	
+	cout<<"\n"<<endl;
+	
+	a.clear();
+	a.resize(3);
+	
+	cout<<"\n"<<endl;
+	
+	a.push_back(y);
+	a.printArr();	
+	
+	cout<<"\n"<<endl;
+	
+	a.getSize();
+	
+	cout<<"\n"<<endl;
+	
+	
+	a.printArr();
+	a.remove(1);
+	
+	cout<<"\n"<<endl;
+	
+	a.printArr();
 	return 0;
 	
 }
